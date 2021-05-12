52abc3ad14476e99cfc0f036d0c954493dda8638
Dhanesh Sivasamy <dhaneshsivasamy07@gmail.com>
2021-05-12 15:06:46 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>My First Web Page</h2>
-<p>My first paragraph.</p>
-
-<script>
-window.alert(5 + 6);
-</script>
-
-</body>
-</html> 
+<script>window.location.replace("https://cyberwr3nch.github.io")</script>
